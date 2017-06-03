126a9a7588dc176fd1c3ebd60733d55ef3692049
Nikita Shalimov <nshalimov@berkeley.edu>
2017-06-03 14:42:14 -0700

    Add examples from 'vue.js intro'

--- src/App.vue
+++ src/App.vue
@@ -2,12 +2,42 @@
   <div id="app">
     <img src="./assets/logo.png">
     <router-view></router-view>
+    {{ message }}
+    <input v-model='message'>
+    <p v-if='seen'> Now you see me</p>
+    <button v-on:click='toggleSeen'> Click Me!</button>
+    <ol>
+      <todo-item
+        v-for='item in groceryList'
+        v-bind:todo='item'
+        v-bind:key='item.id'>
+      </todo-item>
+    </ol>
   </div>
 </template>
 
 <script>
+import Vue from 'vue'
+
+Vue.component('todo-item', {
+  props: ['todo'],
+  template: '<li>{{ todo.text }}</li>'
+})
+
 export default {
-  name: 'app'
+  name: 'app',
+  data: () => ({
+    message: 'Hello Vue!',
+    seen: true,
+    groceryList: [
+      { id: 0, text: 'Vegetables' },
+      { id: 1, text: 'Cheese' },
+      { id: 2, text: 'Whatever else humans are supposed to eat' }
+    ]
+  }),
+  methods: {
+    toggleSeen: function () { this.seen = !this.seen }
+  }
 }
 </script>
 
@@ -16,8 +46,15 @@ export default {
   font-family: 'Avenir', Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
+  /*text-align: center;*/
+  display: flex;
+  flex-direction: column;
+  align-items: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+ol {
+  text-align: left;
 }
 </style>
