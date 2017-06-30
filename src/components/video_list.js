1e5242a984c29cb9458328ce1d266561cc1e5d2a
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-30 14:38:06 -0700

    Start videoListItem

--- src/components/video_list.js
+++ src/components/video_list.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import VideoListItem from './video_list_item';
 
 const VideoList = ({ videos }) => {
+  const videoItems = videos.map(video => (
+    <VideoListItem key={video.etag} video={video} />
+  ));
+
   return (
     <ul className='col-md-4 list-group'>
-      {videos.length}
+      {videoItems}
     </ul>
   );
 };
