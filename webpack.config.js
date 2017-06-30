0ddfabc29d78c23fd144c85eea6a2840d15bd72e
Nikita Shalimov <ndshalimov@gmail.com>
2017-06-30 13:24:34 -0700

    Add webpack config

---
+++ webpack.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  context: __dirname,
+  entry: './src/index.js',
+  output: {
+    path: __dirname,
+    publicPath: '/',
+    filename: 'bundle.js'
+  },
+  module: {
+    loaders: [
+      {
+        test:/\.jsx?$/,
+        exclude: /node_modules/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
