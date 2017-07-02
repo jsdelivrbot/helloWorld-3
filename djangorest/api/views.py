fa0cd082e1ba12544bf511d7ce70b0ced56d8b79
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-02 14:15:23 -0700

    Add DetailsView and urlpatterns

--- djangorest/api/views.py
+++ djangorest/api/views.py
@@ -3,10 +3,15 @@ from .serializers import BucketlistSerializer
 from .models import Bucketlist
 
 class CreateView(generics.ListCreateAPIView):
-    """This class defines the create behaviour of the REST API"""
+    """This class handles POST requests to the API"""
     queryset = Bucketlist.objects.all()
     serializer_class = BucketlistSerializer
 
     def perform_create(self, serializer):
         """Save the post data when creating a new bucketlist"""
         serializer.save()
+
+class DetailsView(generics.RetrieveUpdateDestroyAPIView):
+    """This class handles GET, PUT and DELETE requests to the API"""
+    queryset = Bucketlist.objects.all()
+    serializer_class = BucketlistSerializer
