b8bfe68d73478c4e5c18273588e28b76a86b4ecd
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-02 13:51:35 -0700

    Write test for POST functionality

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,4 +1,7 @@
 from django.test import TestCase
+from django.core.urlresolvers import reverse
+from rest_framework import status
+from rest_framework.test import APIClient
 from .models import Bucketlist
 
 class ModelTestCase(TestCase):
@@ -6,7 +9,7 @@ class ModelTestCase(TestCase):
 
     def setUp(self):
         """Define test client"""
-        self.bucketlist_name="Follow TDD principles"
+        self.bucketlist_name = "Follow TDD principles"
         self.bucketlist = Bucketlist(name=self.bucketlist_name)
 
     def test_model_can_create_a_bucketlist(self):
@@ -15,3 +18,19 @@ class ModelTestCase(TestCase):
         self.bucketlist.save()
         new_count = Bucketlist.objects.count()
         self.assertNotEqual(old_count, new_count)
+
+class ViewTestCase(TestCase):
+    """Test suite for the api views"""
+
+    def setUp(self):
+        """Define test client"""
+        self.client = APIClient()
+        self.bucketlist_data = {'name': 'get good'}
+        self.response = self.client.post(
+            reverse('create'),
+            self.bucketlist_data,
+            format='json')
+
+    def test_api_can_create_a_bucketlist(self):
+        """Test the api has bucket creation capability"""
+        self.assertEqual(self.response.status_code, status.HTTP_201_CREATED)
