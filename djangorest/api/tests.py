0a64b8f8d74b22f7f8d7dee0f08404a59131ce63
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-02 13:22:22 -0700

    Write basic test for Bucketlist model

--- djangorest/api/tests.py
+++ djangorest/api/tests.py
@@ -1,3 +1,17 @@
 from django.test import TestCase
+from .models import Bucketlist
 
-# Create your tests here.
+class ModelTestCase(TestCase):
+    """Test suite for the Bucketlist model"""
+
+    def setUp(self):
+        """Define test client"""
+        self.bucketlist_name="Follow TDD principles"
+        self.bucketlist = Bucketlist(name=self.bucketlist_name)
+
+    def test_model_can_create_a_bucketlist(self):
+        """Test the bucketlist model can create a bucketlist instance"""
+        old_count = Bucketlist.objects.count()
+        self.bucketlist.save()
+        new_count = Bucketlist.objects.count()
+        self.assertNotEqual(old_count, new_count)
