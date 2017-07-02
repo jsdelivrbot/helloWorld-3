68cf2d0380d4be92c9f4891a41b8fb1e3dd56b29
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-01 18:51:25 -0700

    Make searchBar input a controlled field

--- src/components/app.js
+++ src/components/app.js
@@ -4,7 +4,9 @@ import SearchBar from '../containers/search_bar';
 export default class App extends Component {
   render() {
     return (
-      <SearchBar />
+      <div>
+        <SearchBar />
+      </div>
     );
   }
 }
