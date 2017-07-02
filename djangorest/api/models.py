9b0c02870c300a180b5ea4b62c1baaa36fb7436f
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-02 13:44:59 -0700

    Add model serializer

--- djangorest/api/models.py
+++ djangorest/api/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 
 class Bucketlist(models.Model):
+    """Bucketlist model"""
     name = models.CharField(max_length=255, blank=False, unique=True)
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
