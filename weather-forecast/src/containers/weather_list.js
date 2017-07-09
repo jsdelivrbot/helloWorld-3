20f0108b06cfce0c929f125230144a2c2e68d94d
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-09 11:02:27 -0700

    Connect WeatherList to redux state

--- weather-forecast/src/containers/weather_list.js
+++ weather-forecast/src/containers/weather_list.js
@@ -2,19 +2,35 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
 class WeatherList extends Component {
+  renderWeather(cityData) {
+    const name = cityData.city.name;
+
+    return (
+      <tr key={name}>
+        <td>{name}</td>
+      </tr>
+    );
+  }
+
   render() {
     return(
       <table className='table table-hover'>
         <thead>
           <tr>
             <th>City</th>
+            <th>Temperature</th>
+            <th>Pressure</th>
+            <th>Humidity</th>
           </tr>
         </thead>
         <tbody>
+          {this.props.weather.map(this.renderWeather)}
         </tbody>
       </table>
     );
   }
 }
 
-export default connect(null, null)(WeatherList);
+const mapStateToProps = ({ weather }) => ({ weather });
+
+export default connect(mapStateToProps)(WeatherList);
