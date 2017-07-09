37f5e59e438478be200dbb318bf48ce273027f1c
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-09 11:20:44 -0700

    Add units to charts

--- weather-forecast/src/components/chart.js
+++ weather-forecast/src/components/chart.js
@@ -1,10 +1,19 @@
 import React from 'react';
-import { Sparklines, SparklinesLine } from 'react-sparklines';
+import _ from 'lodash';
+import {
+  Sparklines,
+  SparklinesLine,
+  SparklinesReferenceLine
+ } from 'react-sparklines';
 
-export default ({data, color}) => (
+const average = data => _.round(_.sum(data)/data.length);
+
+export default ({data, units, color}) => (
   <div>
     <Sparklines height={120} width={180} data={data}>
       <SparklinesLine color={color} />
+      <SparklinesReferenceLine type='avg' />
     </Sparklines>
+    <div>{average(data)} {units}</div>
   </div>
 );
