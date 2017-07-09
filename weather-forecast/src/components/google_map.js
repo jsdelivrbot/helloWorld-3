500a8870b8e6ebc7f2ad688226c3f1b1e848b7e0
Nikita Shalimov <ndshalimov@gmail.com>
2017-07-09 11:32:47 -0700

    Start googleMap component

--- weather-forecast/src/components/google_map.js
+++ weather-forecast/src/components/google_map.js
@@ -1,7 +1,18 @@
 import React, { Component } from 'react';
 
 class GoogleMap extends Component {
+  componentDidMount() {
+    const { lat, lng } = this.props;
 
+    new google.maps.Map(this.refs.map, {
+      zoom: 12,
+      center: { lat, lng }
+    });
+  }
+
+  render() {
+    return <div ref='map' />;
+  }
 }
 
 export default GoogleMap;
--- weather-forecast/src/containers/weather_list.js
+++ weather-forecast/src/containers/weather_list.js
@@ -1,17 +1,19 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import GoogleMap from '../components/google_map';
 import Chart from '../components/chart';
 
 class WeatherList extends Component {
   renderWeather(cityData) {
     const name = cityData.city.name;
+    const { lat, lon } = cityData.city.coord;
     const temps = cityData.list.map(weather => weather.main.temp);
     const pressures = cityData.list.map(weather => weather.main.pressure);
     const humidities = cityData.list.map(weather => weather.main.humidity);
 
     return (
       <tr key={name}>
-        <td>{name}</td>
+        <td><GoogleMap lat={lat} lng={lon}/></td>
         <td><Chart data={temps} color='orange' units='K'/></td>
         <td><Chart data={pressures} color='green' units='hPa'/></td>
         <td><Chart data={humidities} color='black' units='%'/></td>
